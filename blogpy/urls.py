0c1fdd787fb8c93105fc3ad9deb56559ce8a2279
morteza70 <morteza.torkashvand70@gmail.com>
2021-08-17 18:50:08 +0430

    configoratin docker

--- blogpy/urls.py
+++ blogpy/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), # http://127.0.0.1:8000/admin/
 ]
