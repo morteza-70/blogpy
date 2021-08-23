4ef626cde5b0543e10cb52f42b56d8603fdfd7ae
morteza70 <morteza.torkashvand70@gmail.com>
2021-08-23 11:20:05 +0430

    edit databe models for blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(UserProfile)
-admin.site.register(Category)
-admin.site.register(Article)
+# admin.site.register(UserProfile)
+# admin.site.register(Category)
+# admin.site.register(Article)
+
+class UserProfilAdmin(admin.ModelAdmin):
+    list_display = ['user', 'avatar', 'description']
+
+admin.site.register(UserProfile, UserProfilAdmin)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'content']
+    list_display = ['title', 'category', 'created_at']
+
+admin.site.register(Article, ArticleAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'cover']
+
+admin.site.register(Category, CategoryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -19,25 +19,19 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.FileField(upload_to='files/user_avatar/', null=False, blank=False)
     description = models.CharField(max_length=512, null=False, blank=False)
-
-    def __str__(self):
-        return self.user.first_name + ' ' + self.user.last_name
-
+    
 class Article(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
     cover = models.FileField(upload_to='files/article/cover', null=False, blank=False)
     content = RichTextField()
     created_at = models.DateTimeField(default=now, null=False, blank=False)
-    category = models.ForeignKey('category', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     author = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.title
+    # majik method
+    # def __str__(self):
+    #     return self.title
 
 class Category(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
     cover = models.FileField(upload_to='files/category_cover', null=False, blank=False)
-
-    def __str__(self):
-        return self.title      # (show category name)
-        # return self.cover.url   (show path cover file in category)
