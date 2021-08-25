4e35e0816570589459183ae4d695c5868b123a0b
morteza70 <morteza.torkashvand70@gmail.com>
2021-08-25 12:06:48 +0430

    add django jet theme

--- blogpy/settings.py
+++ blogpy/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ckeditor',
     'blog',
+    'jet',
+    'django.contrib.admin',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
