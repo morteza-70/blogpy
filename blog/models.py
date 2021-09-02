90c875cfab173977155ad33ff2ab1776a5724cdc
morteza70 <morteza.torkashvand70@gmail.com>
2021-09-02 18:49:51 +0430

    edit

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ def validate_file_extension(values):
     from django.core.exceptions import ValidationError
 
     ext = os.path.splitext(values.name)[1]
-    valid_extensions = ['.jpg', '.png']
+    valid_extensions = ['.jpg', '.png', 'jpeg']
     if not ext.lower() in valid_extensions:
         raise ValidationError('Unsupported file extention')
 
--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ class IndexPage(TemplateView):
     
     def get(self, request, **kwargs):
         article_data = []
-        all_articles = Article.objects.order_by('-created_at').all()[:6]
+        all_articles = Article.objects.order_by('-created_at').all()[:9]
 
         for article in all_articles:
             article_data.append({
