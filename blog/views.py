db7cecd4edac028edd2b4d331e3d7f4af654b352
morteza70 <morteza.torkashvand70@gmail.com>
2021-09-06 11:54:31 +0430

    dynamin home page

--- blog/views.py
+++ blog/views.py
@@ -16,9 +16,22 @@ class IndexPage(TemplateView):
                 'category': article.category.title,
                 'created_at': article.created_at.date(),
             })
+        
+        promote_data = []
+        all_promote_articles = Article.objects.filter(promote=True)
+        for promote_articles in all_promote_articles:
+            promote_data.append({
+                'category': promote_articles.category.title,
+                'title': promote_articles.title,
+                'cover': promote_articles.cover.url if promote_articles.author.avatar else None,
+                'author': promote_articles.author.user.first_name + ' ' + promote_articles.author.user.last_name,
+                'avatar': promote_articles.author.avatar.url if promote_articles.author.avatar else None,
+                'created_at': promote_articles.created_at.date(),
+            })
 
         context = {
             'article_data': article_data,
+            'promote_articles_data': promote_data,
         }
 
         return render(request,'index.html', context)
